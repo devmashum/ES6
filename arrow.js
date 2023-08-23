24afd4e076c90754a25d0ec7e264fccb05df41fd
devmashum <dev.mashum@gmail.com>
2023-08-23 11:36:08 +0200

    new commit

--- arrow.js
+++ arrow.js
@@ -1,13 +1,17 @@
-function add(a, b) {
-    const result = a + b;
-    if (result > 10) {
-        console.log('The value is grater then 10');
-        return result;
-    } else {
-        console.log('no valid result');
-    }
+// function add(a, b) {
+//     const result = a + b;
+//     if (result > 10) {
+//         console.log('The value is grater then 10');
+//         return result;
+//     } else {
+//         console.log('no valid result');
+//     }
 
-}
+// }
 
-const sum = add(5, 4);
+// const sum = add(5, 4);
+
+const add = (a, b) => a * b;
+const sum = add(5, 5);
+console.log(sum);
 
