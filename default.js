78207716a1346a8828f9fffe10167657531fe9fc
devmashum <dev.mashum@gmail.com>
2023-08-23 10:46:56 +0200

    first commit

---
+++ default.js
@@ -0,0 +1,10 @@
+
+// Default value
+function add(num1 = 10, num2) {
+    const result = num1 + num2;
+
+    console.log(num1, num2, result);
+
+}
+
+const faisal = add(undefined, 3);
---
+++ template.js
@@ -0,0 +1,8 @@
+const first = 'Faisal'
+const second = 'Ahmed'
+const third = 'Mashum'
+
+const name = `${first}
+${second}
+${third}`;
+console.log(name);
