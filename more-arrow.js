dc2b1561cfc88ad7ed9d0395b32e9d4375f59520
devmashum <dev.mashum@gmail.com>
2023-08-23 12:06:57 +0200

    new commit

--- more-arrow.js
+++ more-arrow.js
@@ -5,13 +5,13 @@
 const doMath = (x, y, z) => {
     const sum = x + y + z;
     const mult = x * y * z;
-    const abc = (x - y) * z;
-    const bcn = x * (y + z);
-    const result = sum + mult + abc + bcn;
+    // const abc = (x - y) * z;
+    // const bcn = x * (y + z);
+    const result = sum + mult;
 
     return result;
 }
 
-const input = doMath(2, 3, 4);
+const input = doMath(3, 3, 3);
 
 console.log(input);
