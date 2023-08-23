5a04cbeaa9ef6c72c3bb62bddd4639d0963e27b2
devmashum <dev.mashum@gmail.com>
2023-08-23 14:02:51 +0200

    new commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+const details = {
+    name: 'faisal',
+    age: 32,
+    height: 1.79,
+}
+
+// const keys = Object.keys(details) + Object.values(details);
+
+const keys = Object.keys(details);
+const values = Object.values(details);
+console.log(keys);
+console.log(values);
