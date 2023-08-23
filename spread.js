a46e921a7a10464b5e6209b1a1e160935169adfe
devmashum <dev.mashum@gmail.com>
2023-08-23 13:43:07 +0200

    new commit

---
+++ spread.js
@@ -0,0 +1,7 @@
+const max = Math.max(10, 100, 2000, 200000);
+console.log(max);
+
+const numbers = [10, 20, 30, 100, 120, 1220, 2030];
+
+const maxNumbers = Math.min(...numbers);
+console.log(maxNumbers);
