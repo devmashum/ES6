33da2f32298fd231621743ef88d6dbd83583cc75
devmashum <dev.mashum@gmail.com>
2023-08-25 15:02:22 +0200

    new commit

---
+++ class.js
@@ -0,0 +1,8 @@
+const products = [
+    { id: 1, name: 'lenovo', price: 65000 },
+    { id: 2, name: 'Samsung', price: 125000 },
+    { id: 3, name: 'Nokia', price: 35000 },
+    { id: 4, name: 'Apple', price: 165000 },
+    { id: 5, name: 'LG', price: 64000 },
+    { id: 6, name: 'Walton', price: 5000 }
+]
