17982589239dc1f9fe1c911371d9ae76d216809a
devmashum <dev.mashum@gmail.com>
2023-08-26 14:15:45 +0200

    new commot

--- class.js
+++ class.js
@@ -1,8 +1,17 @@
-const products = [
-    { id: 1, name: 'lenovo', price: 65000 },
-    { id: 2, name: 'Samsung', price: 125000 },
-    { id: 3, name: 'Nokia', price: 35000 },
-    { id: 4, name: 'Apple', price: 165000 },
-    { id: 5, name: 'LG', price: 64000 },
-    { id: 6, name: 'Walton', price: 5000 }
-]
+// const products = [
+//     { id: 1, name: 'lenovo', price: 65000 },
+//     { id: 2, name: 'Samsung', price: 125000 },
+//     { id: 3, name: 'Nokia', price: 35000 },
+//     { id: 4, name: 'Apple', price: 165000 },
+//     { id: 5, name: 'LG', price: 64000 },
+//     { id: 6, name: 'Walton', price: 5000 }
+// ]
+
+
+function min(nums) { return Math.min(nums) }
+console.log(min([1, 3, 2]));
+
+const cube = x => x * x * x;
+console.log(cube(2))
+const [a, b] = [1, 2, 3, 4, 45, 5];
+console.log(a + b);
